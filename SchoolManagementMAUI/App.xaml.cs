818b9444e7c5933a81e4f3362c28d5faeab46bf2
Kenia25Soares <keniadefreitasmorais@gmail.com>
2025-08-26 00:08:36 +0100

    Add project files.

---
+++ SchoolManagementMAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SchoolManagementMAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SchoolManagementMAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SchoolManagementMAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
